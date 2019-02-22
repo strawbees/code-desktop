c2170d6e27bf50f88dcaabbfd01487db43b36635
Murilo Polese <murilopolese@gmail.com>
2019-02-22 10:38:03 +0000

    Autoupdate notifications and compiler

--- src/main.js
+++ src/main.js
@@ -5,10 +5,36 @@ process.on('SIGHUP', () => process.exit())
 process.on('SIGINT', () => process.exit()) // catch ctrl-c
 process.on('SIGTERM', () => process.exit()) // catch kill
 
+const showNotification = async () => {
+	return new Promise((resolve, reject) => {
+		const options = {
+			icon : 'nwjs-assets/icon.png',
+			body : 'Click here to restart'
+		}
+		const notification = new Notification('Update ready to install', options)
+		const timer = setTimeout(() => {
+			notification.close()
+			reject(new Error('AUTOUPDATE: Notification timed out'))
+		}, 5000)
+		notification.onclick = () => {
+			notification.close()
+			clearTimeout(timer)
+			resolve()
+		}
+		notification.onclose = () => {
+			clearTimeout(timer)
+			reject(new Error('AUTOUPDATE: User closed the notification.'))
+		}
+	})
+}
 
 // Start autoupdater
 const AutoUpdater = require('@strawbees/desktop-autoupdater')
 const pkg = require('./package.json')
+if (!pkg.version) {
+	console.log("No version on current app, don't run updater")
+	return
+}
 const updater = new AutoUpdater(pkg)
 updater.addListener('up-to-date', () => {
 	console.log('App is up to date')
@@ -19,6 +45,7 @@ updater.addListener('ready-to-restart', () => {
 })
 updater.addListener('update-available', () => {
 	console.log('Update Available')
+	showNotification()
 })
 updater.addListener('error', (err) => {
 	console.error('Updater error:', err)
