45c1aa396de1b78c758c514eb5dfa2a150abd582
Paulo Barcelos <paulo@azucrina.org>
2019-07-11 12:17:06 +0200

    new way of starting the compiler

--- src/inject_js_start.js
+++ src/inject_js_start.js
@@ -1,20 +1,2 @@
-const { fork } = require('child_process')
-
-// Start the compiler
-process.env.COMPILER_ROOT_DIR = window.nw.App.dataPath
-process.env.COMPILER_PORT = 9511
-const compilerProcess = fork(
-	require.resolve('@strawbees/code-compiler-service/app.js'),
-	[],
-	{
-		silent : false,
-		stdio: 'pipe'
-	}
-)
-compilerProcess.stdout.on('data', data =>
-	console.log(`COMPILER stdout: ${data}`)
-)
-compilerProcess.stderr.on('data', data =>
-	console.log(`COMPILER stderr: ${data}`)
-)
-process.on('exit', () => compilerProcess.kill())
+// starts the compiler with the correct data that
+process.mainModule.exports.startCompiler(window.nw.App.dataPath)
